13c88a12f7ac04910dafba19051f636a99df8e72
Nazik5436 <166996612+Nazik5436@users.noreply.github.com>
2026-03-29 20:51:45 +0300

    Header.vue & reset.scss & variables.scss

---
+++ TZ-vue-project/src/components/Header.vue
@@ -0,0 +1,82 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header class="header">
+    <div class="container">
+      <RouterLink to="/" class="logo">
+        LOGO
+      </RouterLink>
+
+      <nav class="nav">
+        <RouterLink to="/" class="nav-link">Home</RouterLink>
+        <a href="#" class="nav-link">About</a>
+        <a href="#" class="nav-link">Service</a>
+        <a href="#" class="nav-link">Blog</a>
+        <a href="#" class="nav-link">Contact</a>
+      </nav>
+
+      <div class="actions">
+        <RouterLink to="/form" class="btn-primary">
+          Go to form
+        </RouterLink>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+.header {
+  width: 100%;
+  background-color: #fff;
+  padding: 20px 0;
+  border-bottom: 1px solid #eee;
+
+  .container {
+    max-width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 15px;
+  }
+
+  .logo {
+    font-size: 24px;
+    font-weight: bold;
+    text-decoration: none;
+    color: $dark-bg; // Використовуємо твою змінну!
+  }
+
+  .nav {
+    display: flex;
+    gap: 30px;
+
+    &-link {
+      text-decoration: none;
+      color: $text-gray; // Твоя змінна
+      font-weight: 500;
+      transition: color 0.3s;
+
+      &:hover {
+        color: $primary-purple;
+      }
+    }
+  }
+
+  .btn-primary {
+    background-color: $primary-purple; // Твоя змінна
+    color: white;
+    padding: 12px 25px;
+    border-radius: 50px;
+    text-decoration: none;
+    font-weight: 600;
+    transition: opacity 0.3s;
+
+    &:hover {
+      opacity: 0.9;
+    }
+  }
+}
+</style>
--- TZ-vue-project/src/views/HomeView.vue
+++ TZ-vue-project/src/views/HomeView.vue
@@ -1,9 +1,16 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
+  <main class="home__page">
+    <h1>Тут буде мій Landing Page</h1>
   </main>
 </template>
+
+<style scoped lang="scss">
+.home__page {
+  background-color: white;
+  
+  h1 {
+    color: $primary-purple;
+    font-size: 24px;
+  }
+}
+</style>
