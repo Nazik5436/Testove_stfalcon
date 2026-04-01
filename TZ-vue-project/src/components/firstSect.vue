3efcd9a5521c8d98f14f59dea3fd4c06ad91eed6
Nazik5436 <166996612+Nazik5436@users.noreply.github.com>
2026-04-01 21:05:18 +0300

    finish firstSect

--- TZ-vue-project/src/components/firstSect.vue
+++ TZ-vue-project/src/components/firstSect.vue
@@ -5,11 +5,11 @@
                 <h1 class="first__right__title">Strong for life with us</h1>
                 <p class="first__right__par">The best Pre/postnatal fitness app! 100 exercises divided in before, during and after pregnancy. Stay strong and healthy through all stages of pregnancy.</p>
                 <div class="first__right__link">
-                    <a href="https://www.apple.com/app-store/" target="_blank" class="first__right__link__item">
-                    <img src="@/assets/img/AppStore.svg" alt="AppStore" >
+                    <a href="https://www.apple.com/app-store/" target="_blank">
+                    <img src="@/assets/img/AppStore.svg" alt="AppStore">
                     </a>
 
-                    <a href="https://play.google.com/" target="_blank" class="first__right__link__item">
+                    <a href="https://play.google.com/" target="_blank">
                     <img src="@/assets/img/GooglePlay.svg" alt="Google play">
                     </a>
                 </div>
@@ -37,35 +37,59 @@
     background-size: cover; 
     background-position: center;
     border-radius: 40px; 
-    padding: 106px 106px;
+    // padding: 106px 0 106px 106px;
+    margin-top: 88px;
 
     .container{
         display: flex;
         align-items: center;
         justify-content: space-between; 
-        gap: 40px;
+        gap: 55px;
 
         .first__right{
+            padding: 106px 0 230px 106px;
 
             &__title{
+                color: $dark-text;
+                max-width: 498px;
+                font-size: 56px;
+                font-weight: 700;
+                line-height: normal;
+                margin-bottom: 58px;
+                text-align: left;
 
+                &::after{
+                    content: ''; 
+                    display: block;     
+                    width: 46px; 
+                    height: 4px; 
+                    background-color: #ff4b60;              
+                    margin-top: 21px;
+                    border-radius: 2px;
+                }
             }
 
             &__par{
-
+                color: $dark-text;
+                max-width: 500px;
+                font-size: 24px;
+                font-style: normal;
+                font-weight: 400;
+                line-height: 160%;
+                margin-bottom: 37px;
             }
 
             &__link{
-
-                &__item{
-                            
-                }
+                display: flex;
+                justify-content: space-between;
+                max-width: 380px;
+                gap: 40px;
             }
 
         }
 
         .first__left{
-
+            padding: 106px 0 0 0;
         }
     }
 }
